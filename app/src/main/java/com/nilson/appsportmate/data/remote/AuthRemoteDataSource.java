4c03df4eb51d9e143e2629b5da46d743bfc3cdda
Jordy Pinos <jordypinos3@gmail.com>
2025-10-13 09:50:12 +0200

    feat: Refactor authentication methods to use alias instead of email

--- app/src/main/java/com/nilson/appsportmate/data/remote/AuthRemoteDataSource.java
+++ app/src/main/java/com/nilson/appsportmate/data/remote/AuthRemoteDataSource.java
@@ -5,25 +5,18 @@ import android.util.Log;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
 import com.nilson.appsportmate.common.utils.Result;
-import com.nilson.appsportmate.domain.models.AuthRole;
-import com.nilson.appsportmate.domain.models.User;
 
 import java.util.Optional;
 
-
 public class AuthRemoteDataSource {
     private final FirebaseAuth auth;
-    private final FirebaseFirestore firestore;
 
     public AuthRemoteDataSource() {
         this.auth = FirebaseAuth.getInstance();
-        this.firestore = FirebaseFirestore.getInstance();
     }
 
-    public Result<User> login(String email, String password) {
+    public Result<String> login(String email, String password) {
         try {
             AuthResult result = auth.signInWithEmailAndPassword(email, password).getResult();
 
@@ -31,39 +24,23 @@ public class AuthRemoteDataSource {
                     .map(FirebaseUser::getUid)
                     .orElseThrow();
 
-            DocumentSnapshot doc = firestore.collection("users")
-                    .document(uid)
-                    .get()
-                    .getResult();
-
-            if (!doc.exists()) {
-                Log.i("AuthRemoteDataSource", "User don't exists");
-                throw new Exception("User don't exists");
-            }
-
-            User user = doc.toObject(User.class);
-            return new Result.Success<>(user);
+            return new Result.Success<>(uid);
         } catch (Exception e) {
             Log.e("AuthRemoteDataSource", "Error login user: " + e.getLocalizedMessage());
             return new Result.Error<>(e);
         }
     }
 
-    public Result<User> signUp(String email, String password, AuthRole role) {
+    public Result<String> signUp(String email, String password) {
         try {
             AuthResult result = auth.createUserWithEmailAndPassword(email, password).getResult();
+
 
             String uid = Optional.ofNullable(result.getUser())
                     .map(FirebaseUser::getUid)
                     .orElseThrow();
 
-            User newUser = new User(uid, "", role);
-
-            firestore.collection("users")
-                    .document(uid)
-                    .set(newUser);
-
-            return new Result.Success<>(newUser);
+            return new Result.Success<>(uid);
         } catch (Exception e) {
             Log.e("AuthRemoteDataSource", "Error sign in user: " + e.getLocalizedMessage());
             return new Result.Error<>(e);
--- app/src/main/java/com/nilson/appsportmate/data/repository/AuthRepositoryImpl.java
+++ app/src/main/java/com/nilson/appsportmate/data/repository/AuthRepositoryImpl.java
@@ -1,43 +1,49 @@
 package com.nilson.appsportmate.data.repository;
 
+import android.util.Log;
+
+import com.nilson.appsportmate.common.utils.AuthAliasHelper;
 import com.nilson.appsportmate.common.utils.Result;
-import com.nilson.appsportmate.data.local.AuthLocalDataSource;
 import com.nilson.appsportmate.data.remote.AuthRemoteDataSource;
-import com.nilson.appsportmate.domain.models.AuthRole;
-import com.nilson.appsportmate.domain.models.User;
 import com.nilson.appsportmate.domain.repository.AuthRepository;
 
 public class AuthRepositoryImpl implements AuthRepository {
     private final AuthRemoteDataSource remoteDataSource;
-    private final AuthLocalDataSource localDataSource;
 
-    public AuthRepositoryImpl(AuthRemoteDataSource remoteDataSource,
-                              AuthLocalDataSource localDataSource) {
+    public AuthRepositoryImpl(AuthRemoteDataSource remoteDataSource) {
         this.remoteDataSource = remoteDataSource;
-        this.localDataSource = localDataSource;
     }
 
     @Override
-    public Result<User> login(String email, String password) {
-        Result<User> remoteResult = remoteDataSource.login(email, password);
+    public Result<String> login(String alias, String password) {
+        String aliasEmail = AuthAliasHelper.aliasToEmail(alias);
+        Result<String> remoteResult = remoteDataSource.login(aliasEmail, password);
 
         return handleAuthResult(remoteResult);
     }
 
     @Override
-    public Result<User> signUp(String email, String password) {
-        Result<User> remoteResult = remoteDataSource.signUp(email, password, AuthRole.USER);
+    public Result<String> signUp(String alias, String password) {
+        String aliasEmail = AuthAliasHelper.aliasToEmail(alias);
+        Result<String> remoteResult = remoteDataSource.signUp(aliasEmail, password);
 
         return handleAuthResult(remoteResult);
     }
 
-    private Result<User> handleAuthResult(Result<User> result) {
-        if (result instanceof Result.Error<User>) {
-            return new Result.Error<>(((Result.Error<User>) result).exception);
+    /**
+     * Handles the result of an authentication operation.
+     *
+     * @param result The result of the authentication operation.
+     * @return A Result containing the uid if successful, or an error if failed.
+     */
+    private Result<String> handleAuthResult(Result<String> result) {
+        if (result instanceof Result.Error<String>) {
+            Log.e("AuthRepositoryImpl", "handleAuthResult: Error " + ((Result.Error<String>) result).exception.getLocalizedMessage());
+            return new Result.Error<>(((Result.Error<String>) result).exception);
         }
 
-        User user = ((Result.Success<User>) result).data;
-        localDataSource.saveUser(user.uid(), user.alias(), user.role().name());
-        return new Result.Success<>(user);
+        String uid = ((Result.Success<String>) result).data;
+        Log.d("AuthRepositoryImpl", "handleAuthResult: Success " + uid);
+        return new Result.Success<>(uid);
     }
 }
